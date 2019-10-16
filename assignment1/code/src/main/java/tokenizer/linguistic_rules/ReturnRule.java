dfa8a59263025b34a3f24c2dab4bc9cb99313cf8
aspedrosa <aspedrosa@ua.pt>
2019-10-16 16:00:10 +0100

    Bug fix on ReturnRule

--- assignment1/code/src/main/java/tokenizer/linguistic_rules/ReturnRule.java
+++ assignment1/code/src/main/java/tokenizer/linguistic_rules/ReturnRule.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class ReturnRule extends LinguisticRule {
 
+    public ReturnRule() {
+        super(null);
+    }
+
     @Override
     public List<String> apply(List<String> toApply) {
         return toApply;
