e9061ecd6bcff62df775febdc1a1421748ec82ea
aspedrosa <aspedrosa@ua.pt>
2019-12-05 13:43:34 +0000

    Created searcher package

---
+++ assignment3/code/src/main/java/searcher/Evaluation.java
@@ -0,0 +1,37 @@
+package searcher;
+
+public class Evaluation {
+
+    private int FP;
+
+    private int FN;
+
+    private int TP;
+
+    private int TN;
+
+    private void calculatePrecision() {
+
+    }
+
+    private void calculateRecall() {
+
+    }
+
+    private void calculateFMeasure() {
+
+    }
+
+    private void calculateMeanAveragePrecision() {
+
+    }
+
+    private void calculateMeanPrecisionAtRank10() {
+
+    }
+
+    private void calculateNDCG() {
+        
+    }
+
+}
---
+++ assignment3/code/src/main/java/searcher/Searcher.java
@@ -0,0 +1,4 @@
+package searcher;
+
+public class Searcher {
+}
