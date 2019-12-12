c11905ff7201780eb0905f7bad35d5acfd47baab
João Rodrigues <joaosilva9@ua.pt>
2019-12-12 15:55:03 +0000

    Correct Evaluation.java

--- assignment3/code/src/main/java/searcher/Evaluation.java
+++ assignment3/code/src/main/java/searcher/Evaluation.java
@@ -1,6 +1,7 @@
 package searcher;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 import java.lang.Math;
 
@@ -19,11 +20,16 @@ public class Evaluation {
      */
     public List<String> docs(String filename){
         List<String> idQueries = new ArrayList<>();
-        Scanner input = new Scanner(new File(filename));
+        Scanner input = null;
+        try {
+            input = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         while (input.hasNextLine()){
-            String[] line = input.nextLine.split("\t");
+            String[] line = input.nextLine().split("\t");
             if (!idQueries.contains(line[0])){
-                idQueries.add(line[0])
+                idQueries.add(line[0]);
             }
         }
         return idQueries;
@@ -34,16 +40,21 @@ public class Evaluation {
      * @param filename
      * @return list of revelance scores
      */
-    public Map<String, ArrayList<String>> loadTruth(String filename){
-        Map<String, Array<ArrayList<String>>> queriesTruth = new HashMap<>();
-        Scanner input = new Scanner(new File(filename));
+    public Map<String, ArrayList<ArrayList<String>>> loadTruth(String filename){
+        Map<String, ArrayList<ArrayList<String>>> queriesTruth = new HashMap<>();
+        Scanner input = null;
+        try {
+            input = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         while (input.hasNextLine()){
-            String[] line = input.nextLine.split("\t");
+            String[] line = input.nextLine().split("\t");
             ArrayList<String> info = new ArrayList<>();
             info.add(line[1]); info.add(line[2]);
 
             ArrayList<ArrayList<String>> infos = new ArrayList<>();
-            if (!queriesTruth.contains(line[0])){
+            if (!queriesTruth.containsKey(line[0])){
                 infos.add(info);
                 queriesTruth.put(line[0], infos);
             }
@@ -98,7 +109,7 @@ public class Evaluation {
         for (int i = 0; i < rank; i++){
             if (docs.contains(results.get(i))){
                 count += 1;
-                ArrayList<Double> calc = calculateMeanPrecision_Recall_FMeasure(docs, results.subList(0, i));
+                ArrayList<Double> calc = calculateMeanPrecision_Recall_FMeasure(docs, (ArrayList<String>) results.subList(0, i));
                 pSum += calc.get(0);
             }
         }
@@ -121,18 +132,18 @@ public class Evaluation {
             for (ArrayList<String> t: truth){
                 if (r.equals(t.get(0))){
                     cond = true;
-                    resultsRevelance.add(t.get(1));
+                    resultsRevelance.add(Double.parseDouble(t.get(1)));
                 }
             }
-            if (!cond) resultsRevelance.add(0);
+            if (!cond) resultsRevelance.add(0.0);
         }
         ArrayList<Double> idealRe = new ArrayList<>();
         idealRe.addAll(resultsRevelance);
         Collections.reverse(idealRe);
         for (int j = 0; j < idealRe.size(); j++){
             double denominator = j+1 > 1 ? Math.log(j+1)/Math.log(2) : 1;
-            ideal += idealRe[j]/denominator;
-            actual += resultsRevelance[j]/denominator;
+            ideal += idealRe.get(j)/denominator;
+            actual += resultsRevelance.get(j)/denominator;
 
         }
         return ideal != 0 ? actual/ideal : 0.0;
@@ -148,7 +159,7 @@ public class Evaluation {
         for (double d: numbers){
             result += d;
         }
-        return d;
+        return result;
     }
 
     /**
@@ -173,23 +184,22 @@ public class Evaluation {
                 docsTrue.add(x.get(0));
             }
 
-            ArrayList<Double> calc = calculateMeanPrecision_Recall_FMeasure(docsTrue, PMIDs);
+            ArrayList<Double> calc = calculateMeanPrecision_Recall_FMeasure(docsTrue, (ArrayList<String>) PMIDs);
 
-            double mprecision10 = calculateMeanPrecisionAtRank10(docsTrue, PMIDs, 10);
-            double calcNDCG = calculateNDCG(truth, PMIDs);
+            double mprecision10 = calculateMeanPrecisionAtRank10(docsTrue, (ArrayList<String>) PMIDs, 10);
+            double calcNDCG = calculateNDCG(truth, (ArrayList<String>) PMIDs);
 
             precisions.add(calc.get(0));
             recalls.add(calc.get(1));
             fmeasures.add(calc.get(2));
-            meanprecisons10.add(meanprecisons10);
+            meanprecisons10.add(mprecision10);
             ndcg.add(calcNDCG);
         }
-        avgPrecisions = sum(precisions)/(precisions.size())
-        avgRecalls = sum(recalls)/(recalls.size())
-        avgFmeasures = sum(fmeasures)/(fmeasures.size())
-        avgPrecisions = sum(precisions)/(precisions.size())
-        avgMeanPrecisions10 = sum(meanprecisons10)/(meanprecisons10.size())
-        avgNDCG = sum(ndcgs)/(ndcgs.size())
+        double avgPrecisions = sum(precisions)/(precisions.size());
+        double avgRecalls = sum(recalls)/(recalls.size());
+        double avgFmeasures = sum(fmeasures)/(fmeasures.size());
+        double avgMeanPrecisions10 = sum(meanprecisons10)/(meanprecisons10.size());
+        double avgNDCG = sum(ndcg)/(ndcg.size());
     }
 
 }
